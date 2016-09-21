01c24b958496aa31cac0135dc20d85d4272f518b
Evgeniy Kutsevol <kesmoney@gmail.com>
2016-09-21 23:56:13 +0300

    cs50

---
+++ caesar.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int main(int argc, string argv[]) {
+
+    if (argc != 2)  {                 //checking  of correct  key
+        printf("print the positive number of key\n"); 
+        return 1;
+    }
+    
+    int key = atoi(argv[1]);          //string to int 
+    
+    string text = GetString();        //take a text
+    
+    
+    for (int i = 0; i < strlen(text); i++) {   
+        
+        if (isalpha(text[i])) {
+            if (isupper(text[i]) == true) {
+                text[i] =(text[i] + key - 'A') % 26 + 'A'; // print cipher text for upper charackers
+                printf("%c", text[i]);
+            } else {
+                text[i] = (text[i] + key - 'a') % 26 + 'a';  // print cipher text for lower charackers
+                printf("%c", text[i]);
+            }
+         
+        } else {
+            printf("%c", text[i]);
+        }
+       
+    }   
+    
+   
+    
+    printf("\n");
+    
+    return 0;
+
+    
+}
